e5a909b58d00e954bff58c187e1ce1193e11d1b2
Justin Yoo <justin.yoojh@gmail.com>
2020-07-12 15:21:33 +0900

    Update Open API integration bits & pieces

--- src/FeedReaders.FunctionApp/Startup.cs
+++ src/FeedReaders.FunctionApp/Startup.cs
@@ -1,26 +1,21 @@
-using Aliencube.AzureFunctions.Extensions.OpenApi;
-using Aliencube.AzureFunctions.Extensions.OpenApi.Abstractions;
-using Aliencube.AzureFunctions.Extensions.OpenApi.Configurations;
-
 using FeedReaders.FunctionApp.Configs;
 using FeedReaders.FunctionApp.Handlers;
 
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
-[assembly: FunctionsStartup(typeof(FeedReaders.FunctionApp.Startup))]
+[assembly: FunctionsStartup(typeof(FeedReaders.FunctionApp.StartUp))]
 namespace FeedReaders.FunctionApp
 {
     /// <summary>
     /// This represents the entity to be invoked during the runtime startup.
     /// </summary>
-    public class Startup : FunctionsStartup
+    public class StartUp : FunctionsStartup
     {
         /// <inheritdoc />
         public override void Configure(IFunctionsHostBuilder builder)
         {
             this.ConfigureConfigurations(builder.Services);
-            this.ConfigureSwagger(builder.Services);
             this.ConfigureHandlers(builder.Services);
             this.ConfigureReaders(builder.Services);
         }
@@ -28,15 +23,6 @@ namespace FeedReaders.FunctionApp
         private void ConfigureConfigurations(IServiceCollection services)
         {
             services.AddSingleton<AppSettings>();
-        }
-
-        private void ConfigureSwagger(IServiceCollection services)
-        {
-            services.AddSingleton<RouteConstraintFilter, RouteConstraintFilter>();
-
-            services.AddTransient<IDocumentHelper, DocumentHelper>();
-            services.AddTransient<IDocument, Document>();
-            services.AddTransient<ISwaggerUI, SwaggerUI>();
         }
 
         private void ConfigureHandlers(IServiceCollection services)
